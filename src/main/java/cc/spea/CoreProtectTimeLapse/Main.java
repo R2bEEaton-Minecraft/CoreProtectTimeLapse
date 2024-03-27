a0d140ae1e1ac3149e875679b669c4cfe6d99822
R2bEEaton <scgryan@gmail.com>
2024-03-27 15:08:53 -0400

    add start of command, some refactoring

--- src/main/java/cc/spea/CoreProtectTimeLapse/Main.java
+++ src/main/java/cc/spea/CoreProtectTimeLapse/Main.java
@@ -9,74 +9,19 @@ import net.coreprotect.CoreProtect;
 import net.coreprotect.CoreProtectAPI;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.broadcastMessage("CoreProtectTimeLapse enabled.");
 
-        FakeCoreProtectAPI api = getCoreProtect();
-        if (api != null) {
-            // TODO:
-            // Need to turn this into a command
-            // Fix entities, tile entities
-            // Look into decay, water spread, etc.
-            // Change start time and end time to absolute times
-            // Stop tick speed
-            // Custom radius
-            // Allow undo at the end
-            // Get list of affected regions for backup restore purposes
-
-            ArrayList<Thread> threadArrayList = new ArrayList<>();
-
-            for (int i = 86400; i < 86400 * 30; i += 86400) {
-                threadArrayList.add(getThread(api, i, 86400));
-            }
-
-            new Thread(() -> {
-                for (Thread thread : threadArrayList) {
-                    thread.start();
-                    try {
-                        thread.join();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }).start();
-        }
+        Objects.requireNonNull(getCommand("cptl")).setExecutor(new CommandManager(this));
+        Objects.requireNonNull(getCommand("cptl")).setTabCompleter(new CommandManager(this));
     }
 
     @Override
     public void onDisable() {
 
-    }
-
-    private Thread getThread(FakeCoreProtectAPI api, int time, int interval) {
-        return new Thread(() -> {
-            api.performRollback(time, interval, null, null, null, null, null, 512, Bukkit.getWorlds().get(0).getSpawnLocation());
-            Bukkit.broadcastMessage("done!");
-        });
-    }
-
-    private FakeCoreProtectAPI getCoreProtect() {
-        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
-
-        // Check that CoreProtect is loaded
-        if (!(plugin instanceof CoreProtect)) {
-            return null;
-        }
-
-        // Check that the API is enabled
-        FakeCoreProtectAPI CoreProtect = new FakeCoreProtectAPI();
-        if (!CoreProtect.isEnabled()) {
-            return null;
-        }
-
-        // Check that a compatible version of the API is loaded
-        if (CoreProtect.APIVersion() < 9) {
-            return null;
-        }
-
-        return CoreProtect;
     }
 }
